de4379fe287e9e8122a45c607ab9e4f5eab209e7
Erik Stone <spologas@gmail.com>
2022-06-20 14:28:17 -0700

    made cards, added some css.

--- experiment-app/src/Shadowrun.js
+++ experiment-app/src/Shadowrun.js
@@ -1,56 +1,63 @@
 import React from "react";
 import Card from "react-bootstrap/Card"
+import "./Shadowrun.css";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="characterData">
       <header className="App-header">
-        <img
+        <img className="shadowrunImg"
           src="https://www.belloflostsouls.net/wp-content/uploads/2019/05/shadowrun-6th-ed-horz.jpg"
           alt="shadowrun-totem 6e"
         />
-        <label>
-            Name/ Primary Alias:
-            <input type="text" name="name" />
-          </label><label>
-            Meta-type:
-            <input type="text" name="name" />
-          </label>
-          <label>
-            Age:
-            <input type="number" name="name" />
-          </label>
-          <label>
-            Height:
-            <input type="text" name="name" />
-          </label>
-          <label>
-            Weight:
-            <input type="text" name="name" />
-          </label>
-          <label>
-            Reputation:
-            <input type="text" name="name" />
-          </label>
-          <label>
-            Heat:
-            <input type="text" name="name" />
-          </label>
-          <label>
-            Karma:
-            <input type="text" name="name" />
-          </label>
-          <label>
-            Total Karma:
-            <input type="text" name="name" />
-          </label>
-          <label>
-            Misc:
-            <input type="text" name="name" />
-          </label>
-          <input type="submit" value="Submit" />
+        <Card className="personalData">
+      <Card.Body className="personalDataBox">
+        <Card.Title>Name/ Primary Alias</Card.Title>
+        {/* Grab info from character creation sheet and show name/ alias here */}
+        <Card.Subtitle>Meta-type</Card.Subtitle>
+        {/* Grab info from character creation sheet and show Meta-type here */}
+        <Card.Subtitle>Age</Card.Subtitle>
+        {/* Grab info from character creation sheet and show Age here */}
+        <Card.Subtitle>Height</Card.Subtitle>
+        {/* Grab info from character creation sheet and show height here */}
+        <Card.Subtitle>Reputation</Card.Subtitle>
+        {/* Grab info from character creation sheet and show Reputation here */}
+        <Card.Subtitle>Heat</Card.Subtitle>
+        {/* Grab info from character creation sheet and show Heat here */}
+        <Card.Subtitle>Karma</Card.Subtitle>
+        {/* Grab info from character creation sheet and show Karma here */}
+        <Card.Subtitle>Total Karma</Card.Subtitle>
+        {/* Grab info from character creation sheet and show Total Karma here */}
+        <Card.Subtitle>Misc</Card.Subtitle>
+        {/* Grab info from character creation sheet and show Misc here */}
+          </Card.Body>
+    </Card>
+    <div className="attributes">
+    <Card>
+  <Card.Body>
+    <Card.Title>Attributes</Card.Title>
+    <Card.Subtitle>Body</Card.Subtitle>
+    {/* Grab info from character creation sheet and show body here */}
+    <Card.Subtitle>Agility</Card.Subtitle>
+    {/* Grab info from character creation sheet and show Agility here */}
+    <Card.Subtitle>Reaction</Card.Subtitle>
+    {/* Grab info from character creation sheet and show Reaction here */}
+    <Card.Subtitle>Strength</Card.Subtitle>
+    {/* Grab info from character creation sheet and show Strength here */}
+    <Card.Subtitle>Willpower</Card.Subtitle>
+    {/* Grab info from character creation sheet and show Willpower here */}
+    <Card.Subtitle>Logic</Card.Subtitle>
+    {/* Grab info from character creation sheet and show Logic here */}
+    <Card.Subtitle>Intuition</Card.Subtitle>
+    {/* Grab info from character creation sheet and show Intuition here */}
+    <Card.Subtitle>Charisma</Card.Subtitle>
+    {/* Grab info from character creation sheet and show Charisma here */}
+    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+  </Card.Body>
+</Card>
+</div>
       </header>
     </div>
   );
